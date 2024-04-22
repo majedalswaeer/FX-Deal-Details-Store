21fb09ea568e45213396f301af0f64552a697435
malswaee <majedalswaeer69@gmail.com>
2024-04-22 22:36:09 +0300

    feat: controller implemented with logging functionality

--- src/main/java/com/ps/warehouse/controllers/DealDetailController.java
+++ src/main/java/com/ps/warehouse/controllers/DealDetailController.java
@@ -1,22 +1,41 @@
 package com.ps.warehouse.controllers;
 
+import com.ps.warehouse.dtos.DealDetail;
 import com.ps.warehouse.entities.DealDetailEntity;
-import com.ps.warehouse.repositories.DealDetailRepository;
+import com.ps.warehouse.mappers.DealDetailMapper;
 import com.ps.warehouse.rest_apis.DealDetailApi;
+import com.ps.warehouse.services.DealDetailService;
+import java.net.URI;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@Slf4j
 @RestController
 public class DealDetailController implements DealDetailApi {
 
-    private final DealDetailRepository dealDetailRepository;
+    private final DealDetailMapper dealDetailMapper;
 
-    public DealDetailController(DealDetailRepository dealDetailRepository) {
-        this.dealDetailRepository = dealDetailRepository;
+    private final DealDetailService dealDetailService;
+
+    public DealDetailController(DealDetailMapper dealDetailMapper, DealDetailService dealDetailService) {
+        this.dealDetailMapper = dealDetailMapper;
+        this.dealDetailService = dealDetailService;
     }
 
     @Override
-    public ResponseEntity<DealDetailEntity> add(DealDetailEntity dealDetail) {
-        return ResponseEntity.ok(dealDetailRepository.save(dealDetail));
+    public ResponseEntity<DealDetail> add(DealDetail dealDetail) {
+        DealDetailEntity dealDetailEntity = dealDetailMapper.dtoTOEntity(dealDetail);
+        DealDetail savedDealDetail = dealDetailService.add(dealDetailEntity);
+        log.info("Deal Detail Created with id : {}", savedDealDetail.getId());
+        return ResponseEntity.created(getLocation(savedDealDetail)).body(savedDealDetail);
+    }
+
+    private URI getLocation(DealDetail dealDetail) {
+        return ServletUriComponentsBuilder.fromCurrentRequest()
+                                          .path("/{id}")
+                                          .buildAndExpand(dealDetail.getId())
+                                          .toUri();
     }
 }
